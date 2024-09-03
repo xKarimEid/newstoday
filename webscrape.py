072d9e8cca8924febe46a69d36d59a4beb6fb66f
karimEid <karimeid93@hotmail.com>
2024-09-02 17:01:56 -0700

    added db sqlite3 to store articles

--- webscrape.py
+++ webscrape.py
@@ -6,22 +6,35 @@ class VGScraper:
         self.url = 'https://www.vg.no'
         #self.urls = []
     
-    def get_html(self):
+    def _get_html(self):
+        """Get the html content of the main page"""
+
         html = urlopen(self.url)
         bs = BeautifulSoup(html, 'html.parser')
         return bs
     
-    def get_urls(self):
-        bs = self.get_html()
-        urls = []
-        for item in bs.find_all('a', {'itemprop': 'url'}):
-            if item['href']:
-               urls.append(item['href'])
-        
-        return urls
+    def get_article_urls(self):
+        """Get all article urls from the main page"""
     
-    def get_article_from_url(self, url):
-        req = urlopen(url)
+        bs = self._get_html()
+        url_data = []
+        for item in bs.find_all('a', {'itemprop': 'url'}):
+            data = {}
+            if item['href']:
+               # Store the article url
+               data['article_url'] = item['href']
+               # Store what newspaper this is
+               data['newspaper'] = 'vg'
+               # Store the article header
+               data['header'] = item.h2.text.replace('\n', '')
+               url_data.append(data)
+
+        return url_data
+    
+    def get_article_from_url(self, article_url):
+        """Get the article text from the article url"""
+    
+        req = urlopen(article_url)
         bs = BeautifulSoup(req, 'html.parser')
         paraghs = []
         for p in bs.find_all('p'):
@@ -32,4 +45,3 @@ class VGScraper:
 
 if __name__ == '__main__':
     s = VGScraper()
-    urls = s.get_urls()
